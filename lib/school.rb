0427f5bd77eafc8351027f33dfd620ae1abd3ee2
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-24 20:09:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+class School 
+  
+  attr_accessor :roster 
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student=(name,grade)
+    @name = name 
+    @grade = name 
+    @roster[@grade]=[]
+    @roster[@grade] << @name 
+  end
+  
+  def add_student(name,grade)
+    @name = name 
+    @grade = grade
+  end 
+  
+  def grade=(grade)
+    @grade
+  end
+  
+  def grade(grade)
+    @grade = grade 
+  end 
+  
+  def sort=(sort)
+    @sort = @roster.name.sort
+  end 
+  
+  def sort 
+    @sort = @roster.name.sort
+  end
+end 
